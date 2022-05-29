389b199826c2cac2bca087e99127532c2768b319
DontStarve72 <helinos67@protonmail.com>
2022-05-28 18:10:18 -0700

    Preliminary give command and change groupid

--- src/main/kotlin/net/helinos/avomod/Logger.kt
+++
@@ -1,6 +0,0 @@
-package net.helinos.avomod
-
-import org.apache.logging.log4j.LogManager
-import org.apache.logging.log4j.Logger as ApacheLogger
-
-object Logger : ApacheLogger by LogManager.getLogger(AvoMod.NAME)
