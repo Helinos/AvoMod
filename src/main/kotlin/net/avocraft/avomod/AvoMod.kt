328cc883f863f3128b0a28e7de910f0b50f58b01
DontStarve72 <helinos67@protonmail.com>
2022-05-28 18:11:14 -0700

    The fuck did I do?

---
+++ src/main/kotlin/net/avocraft/avomod/AvoMod.kt
@@ -0,0 +1,26 @@
+package net.avocraft.avomod
+
+import org.bukkit.event.HandlerList
+import org.bukkit.plugin.java.JavaPlugin
+
+class AvoMod : JavaPlugin() {
+
+    companion object {
+        const val NAME = "AvoMod"
+        const val VERSION = "1.0.0"
+        const val ALIAS = "$NAME $VERSION"
+    }
+
+    override fun onEnable() {
+        server.pluginManager.registerEvents(NoteblockCanceller, this)
+        server.pluginManager.registerEvents(BlockPlacer, this)
+        this.getCommand("avgive")?.setExecutor(CommandGive)
+        Logger.info("$ALIAS enabled")
+    }
+
+    override fun onDisable() {
+        // todo: idk if paper does this already
+        HandlerList.unregisterAll(this)
+        Logger.info("$ALIAS disabled")
+    }
+}
